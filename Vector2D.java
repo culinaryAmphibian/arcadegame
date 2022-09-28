0b9a298fc2817ba4f9186c1670fb64f5d466afbf
culinaryAmphibian <teperbudget@gmail.com>
2022-09-28 09:08:35 -0400

    actual initial commit

--- Vector2D.java
+++ Vector2D.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Vector2D {
+import java.lang.Math;
+
+public class Vector2D {
+
+    public double[] coords = {0.0, 0.0};
+
+    public Vector2D(double x, double y) {
+        coords[0] = x_coord;
+        coords[1] = y_coord;
+    }
+
+    public Vector2D(double x_coord) {
+        coords[0] = x_coord;
+        coords[1] = 0.0;
+    }
+
+    public Vector2D() {
+        coords[0] = 0.0;
+        coords[1] = 0.0;
+    }
+
+    private void reset(double x_coord, double y_coord) {
+        coords[0] = x_coord;
+        coords[1] = y_coord;
+    }
+
+    private double magnitude() {
+        return Math.sqrt(coords[0]*coords[0] + coords[1]*coords[1]);
+    }
+
+    private void scale(double a) {
+        coords[0] *= a;
+        coords[1] *= a;
+    }
+
+    private void addEq(float addend) {
+        coords[0] += addend;
+        coords[1] += addend;
+    }
+
+    private void addEq(Vector2D v) {
+        coords[0] += v[0];
+        coords[1] += v[1];
+    }
+
+    private double angle() {
+        return Math.atan2(coords[1],coords[0]);
+    }
+
+    private void rotate(double angle) {
+
+    }
 }
--- gameLoop.java
+++ gameLoop.java
@@ -1,2 +1,87 @@
-package PACKAGE_NAME;public class gameLoop {
-}
+import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.scene.input.KeyEvent;
+import javafx.util.Duration;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+
+public class GameLoop extends Application
+{
+    @Override
+    public void start(Stage primaryStage) {
+        final double HEIGHT = 300.0, WIDTH = 400.0;
+        primaryStage.setTitle("hi");
+        Group root = new Group();
+        Scene primaryScene = new Scene(root);
+        primaryStage.setScene(primaryScene);
+        Canvas canvas = new Canvas(400, 300);
+        root.getChildren().add(canvas);
+        ArrayList<String> input = new ArrayList<String>();
+        primaryScene.setOnKeyPressed(
+                new EventHandler<KeyEvent>() {
+                    public void handle(KeyEvent e) {
+                        String code = e.getCode().toString();
+                        if (!input.contains(code)) {
+                            input.add(code);
+                        }
+                    }
+                }
+        );
+        primaryScene.setOnKeyReleased(
+                new EventHandler<KeyEvent>() {
+                    public void handle(KeyEvent e) {
+                        String code = e.getCode().toString();
+                        input.remove(code);
+                    }
+                }
+        );
+        final GraphicsContext ctx = canvas.getGraphicsContext2D();
+        ctx.setFill(Color.BLACK);
+        ctx.fillRect(0.0, 0.0, WIDTH, HEIGHT);
+        ctx.setFill(Color.WHITE);
+        ctx.setLineWidth(1);
+        Font mainFont = Font.font("Courier Sans MS", FontWeight.THIN, 20);
+        ctx.setFont(mainFont);
+        ctx.fillText("hello", WIDTH/2, HEIGHT/2);
+        ArrayList<Double> velocity = new ArrayList<Double>();
+        velocity.add((Double) 1.0);
+        velocity.add((Double) 1.0);
+        final long startNanoTime = System.nanoTime();
+        Text word = new Text(10, 50, "hello");
+        root.getChildren().add(word);
+        word.setSelectionFill(Color.BLACK);
+        word.setFont(new Font(20));
+        ctx.setFill(Color.WHITE);
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        new AnimationTimer() {
+            public void handle(long currentNanoTime) {
+                if (word.getX() + word.getLayoutBounds().getWidth() + velocity.get(0) > WIDTH || word.getX() + velocity.get(0) < 0) {
+                    velocity.set(0, velocity.get(0) * -1);
+                }
+                if (word.getY() + word.getLayoutBounds().getHeight()/2 + velocity.get(1) - 10 > HEIGHT || word.getY() + velocity.get(1) - 15 < 0) {
+                    velocity.set(1, velocity.get(1) * -1);
+                }
+                word.setX(word.getX() + velocity.get(0));
+                word.setY(word.getY() + velocity.get(1));
+            }
+        }.start();
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+} 

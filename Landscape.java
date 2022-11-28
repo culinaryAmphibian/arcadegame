c7943c645b98b6ea110e74534806c3fd4cf28e49
Felix g <teperbudget@gmail.com>
2022-11-28 09:30:26 -0500

    i did nothing

--- Landscape.java
+++ Landscape.java
@@ -14,19 +14,17 @@ public class Landscape {
         int oldArrayLen = data == null ? 0 : data.length;
         int[] newArr = new int[oldArrayLen + blockLength];
         if (!direction) {
-            for (int i = oldArrayLen - 1; i >= 0; i--) {
-                newArr[i + blockLength] = data[i];
+            if (data != null) {
+                for (int i = data.length - 1; i >= 0; i--) newArr[i + blockLength] = data[i];
             }
-            for (int i = blockLength - 1; i >= 0; i--) {
-                newArr[i] = (int) (Math.random() * 30) - 15 + newArr[i + 1];
-            }
+
+            for (int i = blockLength - 1; i >= 0; i--) newArr[i] = (int) (Math.random() * 30) - 15 + newArr[i + 1];
         } else {
-            for (int i = 0; i < oldArrayLen; i++) {
-                newArr[i] = data[i];
+            if (data != null) {
+                for (int i = 0; i < data.length; i++) newArr[i] = data[i];
             }
-            for (int i = oldArrayLen; i < oldArrayLen + blockLength; i++) {
-                newArr[i] = (int) (Math.random() * 30) - 15 + (oldArrayLen == 0 ? (int) (Math.random() * 30) - 15 : newArr[i - 1]);
-            }
+            for (int i = oldArrayLen; i < oldArrayLen + blockLength; i++)
+                newArr[i] = (int) (Math.random() * 30) - 15 + (data == null ? (int) (Math.random() * 30) - 15 : newArr[i - 1]);
         }
         data = newArr;
     }
@@ -34,6 +32,9 @@ public class Landscape {
         return data;
     }
     public void drawLandscape(Graphics g, Vector2D pos) {
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+        }
         int[] bounds = {(int) pos.getX() - blockLength, (int) pos.getX() + blockLength};
         int alt = (int) pos.getY();
         for (int i = bounds[0] + blockLength; i <= bounds[1]/2; i++) {
